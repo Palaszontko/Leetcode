70607c27fae318ee3192eb3b71dcf8620af9ec1e
Olgierd Palasz <olgierd.palasz@gmail.com>
2024-02-03 23:42:06 +0100

    128. Solved - Improved

--- ArraysAndHashing/LongestConsecutiveSequence/LongestConsecutiveSequence.go
+++ ArraysAndHashing/LongestConsecutiveSequence/LongestConsecutiveSequence.go
@@ -7,26 +7,20 @@ func LongestConsecutive(nums []int) int {
 		hash_map[val] = true
 	}
 
-	list_of_starts := make([]int, 0)
-
-	for _, val := range nums {
-		if !hash_map[val-1] {
-			list_of_starts = append(list_of_starts, val)
-		}
-	}
-
 	best_count := 0
 
-	for _, val := range list_of_starts {
-		tmp := val
-		count := 1
+	for _, val := range nums {
 
-		for hash_map[tmp+1] {
-			count += 1
-			tmp += 1
+		if !hash_map[val - 1] {
+			tmp := val
+			count := 1
+			for (hash_map[tmp + 1]) {
+				tmp += 1
+				count += 1
+			}
+
+			best_count = max(best_count, count)
 		}
-
-		best_count = max(best_count, count)
 	}
 
 	return best_count
