c1752d6cff5236d8a3086fbca3fbe646acab3f98
Olgierd Palasz <olgierd.palasz@gmail.com>
2024-02-05 20:41:28 +0100

    155. Solved - Lint ignore

--- Stack/MinStack/MinStack.go
+++ Stack/MinStack/MinStack.go
@@ -3,21 +3,22 @@ package MinStack
 import "fmt"
 
 type MinStack struct {
-	values []int
-	top    int
+	values    []int
+	top       int
 	minValues []int
 }
 
 func Constructor() MinStack {
 	return MinStack{
-		values: make([]int, 0),
-		top:    -1,
+		values:    make([]int, 0),
+		top:       -1,
 		minValues: make([]int, 0),
 	}
 }
 
+//lint:file-ignore ST1006 leetcode use "this"
 func (this *MinStack) Push(val int) {
-	if this.top == - 1{
+	if this.top == -1 {
 		this.values = append(this.values, val)
 		this.minValues = append(this.minValues, val)
 		this.top += 1
@@ -25,28 +26,28 @@ func (this *MinStack) Push(val int) {
 		this.values = append(this.values, val)
 		this.top += 1
 
-		if (val < this.GetMin() ) {
+		if val < this.GetMin() {
 			this.minValues = append(this.minValues, val)
 		} else {
-			this.minValues = append(this.minValues, this.minValues[len(this.minValues) - 1])
+			this.minValues = append(this.minValues, this.minValues[len(this.minValues)-1])
 		}
 	}
 }
 
 func (this *MinStack) Pop() {
-    this.values = this.values[:len(this.values) - 1]
-    this.minValues = this.minValues[:len(this.minValues) - 1]
+	this.values = this.values[:len(this.values)-1]
+	this.minValues = this.minValues[:len(this.minValues)-1]
 	this.top -= 1
 }
 
 func (this *MinStack) Top() int {
-	return this.values[len(this.values) - 1]
+	return this.values[len(this.values)-1]
 }
 
 func (this *MinStack) GetMin() int {
-	return this.minValues[len(this.minValues) - 1]
+	return this.minValues[len(this.minValues)-1]
 }
 
-func (this *MinStack) Display() { 
+func (this *MinStack) Display() {
 	fmt.Println(this.values)
 }
