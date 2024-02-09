377a196c7bb246d4bf0c40aaf21943986aa1e386
Olgierd Palasz <olgierd.palasz@gmail.com>
2024-02-09 13:44:35 +0100

    167. Solved - Remove logs

--- TwoPointers/TwoSumIIInputArrayIsSorted/TwoSumIIInputArrayIsSorted.go
+++ TwoPointers/TwoSumIIInputArrayIsSorted/TwoSumIIInputArrayIsSorted.go
@@ -1,15 +1,11 @@
 package twosumiiinputarrayissorted
 
-import "fmt"
-
 func TwoSum(numbers []int, target int) []int {
 
 	i := 0
 	j := len(numbers) - 1
 
 	for numbers[i]+numbers[j] != target {
-
-		fmt.Println(numbers[i], numbers[j])
 		if numbers[i]+numbers[j] > target {
 			j -= 1
 		} else {
