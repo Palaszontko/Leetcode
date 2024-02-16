16de8fd0c156abb2f0b27c697c729c4dcac8251b
Olgierd Palasz <olgierd.palasz@gmail.com>
2024-02-16 11:29:18 +0100

    Linked List Init

---
+++ LinkedList/ReverseLinkedList/ReverseLinkedList.go
@@ -0,0 +1,18 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+package reverselinkedlist
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func ReverseList(head *ListNode) *ListNode {
+
+}
