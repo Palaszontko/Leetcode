078bdeb34ef4b0d9ef8ac868ef26d6e2646c100c
Olgierd Palasz <olgierd.palasz@gmail.com>
2024-04-04 23:29:27 +0200

    First idea

---
+++ BinarySearch/KokoEatingBananas/KokoEatingBananas.go
@@ -0,0 +1,16 @@
+package kokoeatingbananas
+
+import (
+)
+
+func MinEatingSpeed(piles []int, h int) int {
+	return 0
+}
+
+func sum(array []int) int{
+	sum := 0
+	for _, val := range array {
+		sum += val
+	}
+	return sum
+}
