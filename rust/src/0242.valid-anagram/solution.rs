f7449fae9c157d83bef8488631f15ad37d8de9c7
Olgierd Palasz <olgierd.palasz@gmail.com>
2026-07-14 22:17:23 +0200

    feat: add solution and test cases for "Valid Anagram" problem

---
+++ rust/src/0242.valid-anagram/solution.rs
@@ -0,0 +1,38 @@
+// Created by Olgierd Palasz at 2026/07/14 21:55
+// leetgo: dev
+// https://leetcode.com/problems/valid-anagram/
+
+use anyhow::Result;
+use leetgo_rs::*;
+
+struct Solution;
+
+// @lc code=begin
+
+impl Solution {
+    pub fn is_anagram(s: String, t: String) -> bool {
+        if s.len() != t.len() {
+            return false;
+        }
+
+        let mut table: [i32; 26] = [0i32; 26];
+
+        for (&a, &b) in s.as_bytes().iter().zip(t.as_bytes()) {
+            table[(a - b'a') as usize] += 1;
+            table[(b - b'a') as usize] += 1;
+        }
+
+        table.iter().all(|&v| v == 0)
+    }
+}
+
+// @lc code=end
+
+fn main() -> Result<()> {
+    let s: String = deserialize(&read_line()?)?;
+    let t: String = deserialize(&read_line()?)?;
+    let ans: bool = Solution::is_anagram(s, t);
+
+    println!("\noutput: {}", serialize(ans)?);
+    Ok(())
+}
