06d1cd5b63cead6b75215c63a49f07e4d30f92f1
Olgierd Palasz <olgierd.palasz@gmail.com>
2026-07-14 21:50:26 +0200

    feat: add solution and test cases for "Contains Duplicate" problem

---
+++ rust/src/0217.contains-duplicate/solution.rs
@@ -0,0 +1,29 @@
+// Created by Olgierd Palasz at 2026/07/14 20:34
+// leetgo: dev
+// https://leetcode.com/problems/contains-duplicate/
+
+use std::collections::HashSet;
+
+use anyhow::Result;
+use leetgo_rs::*;
+
+struct Solution;
+
+// @lc code=begin
+
+impl Solution {
+    pub fn contains_duplicate(nums: Vec<i32>) -> bool {
+        let mut seen: HashSet<i32> = HashSet::with_capacity(nums.len());
+        nums.into_iter().any(|n| !seen.insert(n))
+    }
+}
+
+// @lc code=end
+
+fn main() -> Result<()> {
+    let nums: Vec<i32> = deserialize(&read_line()?)?;
+    let ans: bool = Solution::contains_duplicate(nums);
+
+    println!("\noutput: {}", serialize(ans)?);
+    Ok(())
+}
