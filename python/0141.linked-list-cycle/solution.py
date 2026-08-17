9225dbd75645ede5acce4a2c2759c1f852cc9842
Olgierd Palasz <olgierd.palasz@gmail.com>
2026-08-17 20:41:27 +0200

    feat: solved 141

---
+++ python/0141.linked-list-cycle/solution.py
@@ -0,0 +1,36 @@
+# Created by Olgierd Palasz at 2026/08/17 20:21
+# leetgo: dev
+# https://leetcode.com/problems/linked-list-cycle/
+
+from typing import *
+from leetgo_py import *
+
+# @lc code=begin
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow = head
+        fast = head
+
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            if slow == fast:
+                return True
+
+        return False
+
+# @lc code=end
+
+# Warning: this is a manual question, the generated test code may be incorrect.
+if __name__ == "__main__":
+    head: ListNode = deserialize("ListNode", read_line())
+    pos: int = deserialize("int", read_line())
+    ans = Solution().hasCycle(head)
+    print("\noutput:", serialize(ans, "boolean"))
